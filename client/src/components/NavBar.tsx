9db2b4ad094edaf347131fa08a37f6e4c2982ced
Drosos <drososD@proton.me>
2024-10-29 18:02:22 +0200

    fix add spaces to the tailwind classes

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { FaBars, FaXmark } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 
 const navLingDecoration =
-  "flex items-center justify-start gap-4 px-2 py-2 hover:rounded-md hover:bg-slate-700";
+  " flex items-center justify-start gap-4 px-2 py-2 hover:rounded-md hover:bg-slate-700 ";
 
 const active = "rounded-md bg-slate-800";
 export default function NavBar() {
@@ -18,7 +18,7 @@ export default function NavBar() {
   return (
     <div className="flex h-16 items-center justify-between bg-slate-950 px-4">
       <div className="text-3xl font-bold">HMS</div>
-      <div className="cursor-pointer p-2 md:hidden" onClick={handleMenu}>
+      <div className="cursor-pointer p-2  md:hidden" onClick={handleMenu}>
         {isMenuOpen ? <FaXmark /> : <FaBars />}
       </div>
       <ul
@@ -26,7 +26,7 @@ export default function NavBar() {
           isMenuOpen
             ? "absolute right-0 top-16 z-10 h-full w-full bg-slate-900"
             : "hidden"
-        }md:static md:flex md:h-auto md:w-auto md:bg-transparent`}
+        } md:static md:flex md:h-auto md:w-auto md:bg-transparent `}
       >
         <li>
           <NavLink
