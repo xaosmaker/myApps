b330de3d0a8dc7a87fadbec2ca52f0d8b1220e86
Drosos <drososD@proton.me>
2025-08-08 06:54:47 +0300

    chore: remove todos store only use api

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import todoSlice from "./todoSlice";
 import countDownslice from "./countDownslice";
 
 export const store = configureStore({
   reducer: {
-    todo: todoSlice,
     countDown: countDownslice,
   },
 });
