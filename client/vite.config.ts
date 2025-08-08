b5273da3dafce4d1ec1091709df06dc7a69c44bf
Drosos Drosinakis <drososD@proton.me>
2025-08-08 20:49:48 +0300

    add monitoring and analyzing tools

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,10 +3,18 @@ import path from "path"
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
 import tailwindcss from '@tailwindcss/vite';
+import { visualizer } from "rollup-plugin-visualizer"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [react(), tailwindcss(), svgr(),
+  visualizer({
+    filename: './dist/stats.html',
+    template: 'treemap', // 'sunburst', 'treemap', or 'network'
+    open: true,          // auto-open after build
+    gzipSize: true,
+    brotliSize: true,
+  })],
   server: {
     allowedHosts: ["myapps"],
     host: "0.0.0.0"
