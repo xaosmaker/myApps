1ab1c72705224f7fd894b0779aa6ad0fecf87ab2
Drosos <drososD@proton.me>
2025-08-29 13:19:33 +0300

    fix: add validations for shift in the backend
    i add validations if the shift is not provided in the backend there is
    an error

--- api/core_apps/work_hours/serializers.py
+++ api/core_apps/work_hours/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from core_apps.work_hours.models import WorkDay, WorkShift
@@ -27,7 +28,17 @@ class WorkDaySerializer(ModelSerializer):
             "comment",
         ]
 
+    def validate(self, attrs):
+        val_attrs = super().validate(attrs)
+        work_day = val_attrs.get("type_of_work_day")
+        shift = val_attrs.get("work_day_shift")
+
+        if work_day in ["Travel", "Work Day"] and not shift:
+            raise serializers.ValidationError({"word_day_shift": "Shift is Required"})
+
+        return val_attrs
+
     def create(self, validated_data):
-        print(validated_data)
+
         user = self.context["request"].user
         return WorkDay.objects.create(**validated_data, profile=user.profile)
