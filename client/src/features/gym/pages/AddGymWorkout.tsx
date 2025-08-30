70fa3028ed1f93f5fcf8bb9f19d179333ac02a5a
Drosos <drososD@proton.me>
2025-08-30 06:05:10 +0300

    refactor: change the ui of the AddGymWorkout

--- client/src/features/gym/pages/AddGymWorkout.tsx
+++ client/src/features/gym/pages/AddGymWorkout.tsx
@@ -1,15 +1,12 @@
 import { useForm } from "react-hook-form";
 import { useGetGymMachine } from "../hooks/useGetGymMachineList";
 import type { GymWorkoutPostType } from "../types/GymTypes";
-import Modal from "../../../components/modal/Modal";
 import Input from "../../../components/Input";
 import AddGymMachine from "../components/AddGymMachine";
 import { usePostGymWorkout } from "../hooks/usePostGymWorkout";
-import Button from "../../../ui/Button";
+import { Button } from "@/components/ui/button";
 import SelectSearch from "@/components/selectSearch/SelectSearch";
-import { Plus } from "lucide-react";
-
-//TODO: need refactor and rething indo step card
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function AddGymWorkout() {
   const { gymMachineList, isGymMachineLoading } = useGetGymMachine();
@@ -36,29 +33,27 @@ export default function AddGymWorkout() {
   const is_timed_machine = gymMachineList.find(
     (item) => item.pkid === machine_id,
   )?.is_tracked_by_time;
-  console.log(machine_id, is_timed_machine);
 
   return (
-    <div>
-      <h2 className="mb-4">AddGymWorkout</h2>
-      <Modal>
-        <form onSubmit={handleSubmit(submitForm)}>
-          <div className="grid grid-cols-[1fr_auto] gap-2">
-            <SelectSearch<GymWorkoutPostType>
-              name="gym_machine_id"
-              control={control}
-              data={gymMachinedata}
-            />
-            <Modal.Open opens="gym">
-              <div className="group/message relative cursor-pointer p-2 text-xl text-green-500">
-                <Plus />
-                <span className="invisible absolute top-9 right-1/2 z-10 translate-x-1/2 rounded-md border-2 border-slate-950 bg-slate-800 px-4 py-2 text-nowrap text-slate-50 transition-all duration-500 group-hover/message:visible">
-                  Add Gym Machine
-                </span>
-              </div>
-            </Modal.Open>
-          </div>
-          <div className="mt-4">
+    <div className="mx-auto mt-10 w-fit">
+      <Card>
+        <CardHeader>
+          <CardTitle>Add Workout</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form
+            onSubmit={handleSubmit(submitForm)}
+            className="flex flex-col gap-6"
+          >
+            <div className="grid grid-cols-[1fr_auto] gap-2">
+              <SelectSearch<GymWorkoutPostType>
+                name="gym_machine_id"
+                label="select gym machine"
+                control={control}
+                data={gymMachinedata}
+              />
+              <AddGymMachine />
+            </div>
             {is_timed_machine === true && (
               <>
                 <Input
@@ -98,13 +93,10 @@ export default function AddGymWorkout() {
                 />
               </>
             )}
-          </div>
-          <Button type="submit">Add Workout</Button>
-        </form>
-        <Modal.Window name="gym">
-          <AddGymMachine />
-        </Modal.Window>
-      </Modal>
+            <Button type="submit">Add Workout</Button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
